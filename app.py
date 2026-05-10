ec477d935f3d5b2448459414dc3b7391a5f482d8
RawanAhmed15 <215084@eru.edu.eg>
2026-05-10 05:26:34 +0300

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 st.set_page_config(page_title="ITI Data Profiler", layout="wide")
 
-st.title("🚀 ITI CICD Project: Professional Data Profiler")
+st.title(" ITI CICD Project: Professional Data Profiler")
 st.markdown("---")
 
 # أولاً: بنحاول نقرأ ملف الداتا اللي جاي مع المشروع
@@ -12,7 +12,7 @@ default_data = "data.csv"
 
 if os.path.exists(default_data):
     df_default = pd.read_csv(default_data)
-    st.subheader("✅ Sample Data Loaded (data.csv)")
+    st.subheader(" Sample Data Loaded (data.csv)")
     st.dataframe(df_default.head())
 
 st.sidebar.header("Operations")
@@ -20,10 +20,10 @@ uploaded_file = st.sidebar.file_uploader("Upload another CSV", type="csv")
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    st.subheader("📊 Analyzed Data View")
+    st.subheader(" Analyzed Data View")
     st.write(df)
     
-    st.subheader("📈 Quick Statistics")
+    st.subheader(" Quick Statistics")
     st.write(df.describe())
     
     st.success("Analysis complete!")
